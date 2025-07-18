d5131e36f52d435b120dfea365a7a0d5ebc23778
cotelCatherine <bezmozggoff@gmail.com>
2025-07-18 22:27:49 +0700

    Initial commit

---
+++ src/main/java/ru/cotelCatherine/listOfRestuarant/ListOfRestaurantApplication.java
@@ -0,0 +1,13 @@
+package ru.cotelCatherine.listOfRestuarant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cotelCatherine/listOfRestuarant/ListOfRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cotelCatherine.listOfRestuarant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
